e5cd022af51417b159f32b214834e35bcc863f73
limduhwan <limduhwan@gmail.com>
2020-03-31 07:28:05 +0900

    S3에 FrontEnd 올리기

--- src/constants/backend-url.js
+++ src/constants/backend-url.js
@@ -1,9 +1,11 @@
 const API_URLs = {
   test: 'http://localhost:3000',
   local: 'http://localhost:3000',
+  production: 'http://c35p-book.s3-website.ap-northeast-2.amazonaws.com',
   // local: 'https://moaapi.c35p.com',
   // dev: 'https://moaapi.c35p.com',
   // stage: 'https://noster-api-stage-910898538.us-west-1.elb.amazonaws.com',
+
 };
 
 console.log('process.env.NODE_ENV', process.env.NODE_ENV);
@@ -11,7 +13,9 @@ console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 let API_SOCK_URL;
 if (process.env.NODE_ENV === 'development') {
   API_SOCK_URL = API_URLs['local'];
-};
+}else if(process.env.NODE_ENV === 'production') {
+  API_SOCK_URL = API_URLs['production'];
+}
 
 export {
   API_SOCK_URL,
