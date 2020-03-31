1e922e19d935aabb4fd5b66f18c27ca51b3d1838
ken <limduhwan@gmail.com>
2020-04-01 08:23:11 +0900

    EC2 호출

--- src/constants/backend-url.js
+++ src/constants/backend-url.js
@@ -7,7 +7,7 @@ const API_URLs = {
 
 };
 
-console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+// console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 
 let API_SOCK_URL;
 if (process.env.NODE_ENV === 'development') {
